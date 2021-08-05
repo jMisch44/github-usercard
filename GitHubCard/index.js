8b128668e401b48ca5b05b54bf0bf02ee018e44e
jMisch44 <jennimisch@gmail.com>
2021-08-05 14:11:40 -0400

    initial push

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,12 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/jMisch44')
+.then( res => {
+  console.log(res);
+}).catch(err => {
+  console.error(err);
+})
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
